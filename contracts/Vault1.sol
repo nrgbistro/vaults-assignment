0d2ddab66bba4e54f943470e4029e3b10db5a70b
Nolan Gelinas <nolangelinas@gmail.com>
2022-01-19 20:16:15 -0500

    Initial Commit

--- contracts/Vault1.sol
+++ contracts/Vault1.sol
@@ -1,6 +1,14 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.4;
+pragma solidity >0.4.0 <0.8.4;
 
-contract Vault1 {
-    
+contract Vault1{
+    uint balance;
+
+    constructor() public {
+        balance = 0;
+    }
+
+    function deposit(uint _amount) public {
+        balance += _amount;
+    }
 }
